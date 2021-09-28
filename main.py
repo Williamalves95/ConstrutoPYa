6cb01e1dd05df7e7a83dfdcae196f7b647b81662
William <williamalves95@hotmail.com>
2021-09-27 22:10:28 -0300

    Uso do parametrize do PyTest. Uso do Try- Except

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # 1 - imports - bibliotecas
 import pytest
 
-
 # 2 - class - classe
 
 def test_somar(numero1, numero2, resultado):
@@ -11,25 +10,35 @@ def test_somar(numero1, numero2, resultado):
 def print_hi(name):
     print(f'Oi, {name}')
 
-
 def somar(numero1, numero2):
     return numero1 + numero2
-
 
 def subtrair(numero1, numero2):
     return numero1 - numero2
 
-
 def multiplicar(numero1, numero2):
     return numero1 * numero2
 
-
+# esse é um exemplo de demonstração
 def dividir(numero1, numero2):
     if numero2 != 0:
         return numero1 / numero2
     else:
-        return "Não dividirás por zero"
+        return 'Não dividirás por zero'
 
+def dividir_try_except(numero1, numero2):
+    try:
+        return numero1 / numero2
+    except:
+        #return 'Não dividirás por zero'
+        if TypeError == ZeroDivisionError:
+            return 'Não dividirás por zero'
+        elif TypeError == ArithmeticError:
+            return 'Erro de cálculo'
+        elif TypeError == ValueError:
+            return 'Erro de valor'
+        else
+            return 'Erro desconhecido'
 
 # teste unitarios / testes de unidades
 
@@ -51,10 +60,14 @@ def test_somar_didatico():
     #valores
     (5, 4, 9), # teste 1
     (3, 2, 5), # teste 2
-    (10,6, 16), # teste 3
+    (10,6, 15), # teste 3
 ])
-def test_somar_compacto(numero1, numero2, resultado):
-    assert somar(numero1,numero2) == resultado
+def test_somar(numero1, numero2, resultado): # ou somar_compacto
+    try:
+        assert somar(numero1,numero2) == resultado
+    except AssertionError:
+        print(f'Entrou no Except: {AssertionError}')
+        pass
 
 def test_somar_resultado_negativo():
     assert somar(-1000, -2000) == -3000
@@ -67,6 +80,21 @@ def test_multiplicar():
 
 def test_dividir():
     assert dividir(8,4) == 2
+
+def test_dividir_por_zero():
+    assert dividir(8,0) == 'Não dividirás por zero'
+
+@pytest.mark.parametrize('numero1, numero2, resultado',[
+    (8,2,4),
+    (20,4,5),
+    (10,0,'Não dividirás por zero' )
+])
+def test_dividir_try_except(numero1, numero2, resultado):
+    assert dividir_try_except(numero1,numero2) == resultado
+
+    #teste positivo --> mostrar o resultado correto
+    #                --> avançar para a próxima eta
+    # teste negativo --> mostrar a mensagem de erro
 
 # Dia 1 : 100 testes : 0 passaram
 # Dia 2 : 100 testes : 5 passaram
